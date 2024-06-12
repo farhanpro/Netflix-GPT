6eb4b7ac5320b67bfeaf91500d967e3994631d4d
farhanpro <shaihfarhan549@gmai.com>
2024-06-12 12:18:10 +0530

    check

--- netflix-gpt/src/utils/firebase.js
+++ netflix-gpt/src/utils/firebase.js
@@ -8,7 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyDLgEYrsSc0PVhpJ7eld96YyWsm4ct61jI",
+  authDomain: "netflixgpt-6c6dd.firebaseapp.com",
+  projectId: "netflixgpt-6c6dd",
+  storageBucket: "netflixgpt-6c6dd.appspot.com",
+  messagingSenderId: "850172755181",
+  appId: "1:850172755181:web:4237dca93ea58d6eabe0e4",
+  measurementId: "G-PW8WZ5JRYJ"
 };
 
 // Initialize Firebase
