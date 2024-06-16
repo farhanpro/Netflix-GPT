90b65b44615e2bcd5bf0249d85c6ca75ca0eb3b9
farhanpro <shaihfarhan549@gmai.com>
2024-06-17 02:28:10 +0530

    Correction

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -92,7 +92,7 @@ signInWithEmailAndPassword(auth, email.current.value,password.current.value)
     return(
     <div>
     <Header/>
-   <div className="relative">
+   <div className="absolute">
    <img alt="Backgroundimage"  src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_small.jpg" srcset="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w"></img>
    </div>
 
